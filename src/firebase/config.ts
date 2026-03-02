b0c868bcc311d2eb5516c6d492f57bf4e8427ca3
stalin robles <stalinrobles@gmail.com>
2026-03-02 00:41:39 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-2214925909-4bcda",
+  "appId": "1:945883453469:web:f076607b4aae7ff210cef4",
+  "apiKey": "AIzaSyDjLvSPA_3jQwZfYO2cp5-Mc3a2Wxh7Dj8",
+  "authDomain": "studio-2214925909-4bcda.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "945883453469"
+};
