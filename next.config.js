1d6bad08bc0a4f86387f83133dda56260ba14dda
stalin robles <stalinrobles@gmail.com>
2026-03-02 03:21:08 +0000

    ERROR: No buildpack groups passed detection. ERROR: failed to detect: bu

--- next.config.js
+++ next.config.js
@@ -1,24 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
+  reactStrictMode: true,
   images: {
     remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'placehold.co',
-      },
-      {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'picsum.photos',
-      },
-      {
-        protocol: 'https',
-        hostname: 'i.imgur.com',
-      },
+      { protocol: 'https', hostname: 'placehold.co' },
+      { protocol: 'https', hostname: 'images.unsplash.com' },
+      { protocol: 'https', hostname: 'picsum.photos' },
+      { protocol: 'https', hostname: 'i.imgur.com' }
     ],
   },
   typescript: {
