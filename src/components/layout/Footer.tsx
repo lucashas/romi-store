74f4723036949e96a87259a2f83ed272ab05e7ed
stalin robles <stalinrobles@gmail.com>
2026-03-03 06:52:27 +0000

    aqui contacto@romi-store.com cambia por orozco.pancho73@hotmail.com

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -37,7 +37,7 @@ export function Footer() {
             </div>
             <div className="flex items-center justify-center gap-2">
               <Mail className="h-4 w-4 text-primary" />
-              <span>Email: contacto@romi-store.com</span>
+              <span>Email: orozco.pancho73@hotmail.com</span>
             </div>
             <div className="flex items-center justify-center gap-2">
               <MapPin className="h-4 w-4 text-primary" />
