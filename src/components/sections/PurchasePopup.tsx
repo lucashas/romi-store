023bd71e191cfb770e2611471869e7a6a912bf41
stalin robles <stalinrobles@gmail.com>
2026-03-04 22:13:34 +0000

    no porque no pones aun la imagen https://imgur.com/Jh61uYJ debajo de ¡SÍ

--- src/components/sections/PurchasePopup.tsx
+++ src/components/sections/PurchasePopup.tsx
@@ -8,7 +8,6 @@ import { Label } from "@/components/ui/label";
 import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
 import { useToast } from "@/hooks/use-toast";
-import Image from "next/image";
 import { useFirestore } from "@/firebase";
 import { collection, addDoc } from "firebase/firestore";
 import { errorEmitter } from "@/firebase/error-emitter";
