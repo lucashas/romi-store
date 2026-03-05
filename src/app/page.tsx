294f39d4cea414f615a070af976f47b3b1f1c99a
stalin robles <stalinrobles@gmail.com>
2026-03-05 04:05:47 +0000

    studio-2214925909:~/studio{main}$ git add Nothing specified, nothing ad

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,5 @@
 import { redirect } from 'next/navigation';
 
 export default function RootPage() {
-  // Redirección directa para evitar procesos innecesarios en el servidor
   redirect('/bioaqua-arroz');
 }
